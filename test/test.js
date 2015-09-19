9e708c59ef0a9c6f63565f1ba62a8d7f893df836
Philipp Alferov <palferov@securesense.ca>
2015-09-19 00:58:24 -0400

    Implement basic tests

--- test/test.js
+++ test/test.js
@@ -14,7 +14,30 @@ describe('parent pointer array to tree', function() {
     });
 
     it('should not modify passed object', function() {
-      expect(current).not.to.be.equal(initial);
+      expect(initial).to.be.deep.equal(initial);
+    });
+
+    it('should return an array', function() {
+      expect(current).to.be.an('array');
+    });
+
+    it('should keep parent_id property', function() {
+      var first = current[0];
+
+      expect(first).to.have.property('parent_id');
+    });
+
+    it('should create nested objects with children', function() {
+      var first = current[0];
+
+      expect(first)
+        .to.have.property('children')
+        .that.is.an('array')
+        .to.have.length.of.at.least(1);
+    });
+
+    it('should return an expected value', function() {
+      expect(current).to.be.deep.equal(expected);
     });
 
   })
