01e16851201ac2da13d76362de708c2b4c381a58
Philipp Alferov <palferov@securesense.ca>
2015-09-19 01:02:24 -0400

    Implement basic library functionality

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+'use strict';
+
+function createTree(list, rootNodes) {
+  var tree = [];
+
+  for (var prop in rootNodes) {
+    if (!rootNodes.hasOwnProperty(prop)) {
+      continue;
+    }
+
+    var node = rootNodes[prop];
+
+    if (list[node.id]) {
+      node.children = createTree(list, list[node.id]);
+    }
+
+    tree.push(node);
+  }
+
+  return tree;
+}
+
+function orderByParents(list) {
+  var parents = {};
+
+  list.forEach(function(item) {
+    var parentID = item.parent_id || 0;
+    parents[parentID] = parents[parentID] || [];
+    parents[parentID].push(item);
+  });
+
+  return parents;
+}
+
 function clone(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
 module.exports = function(obj) {
   var cloned = clone(obj);
-
-  return cloned;
+  var ordered = orderByParents(cloned);
+  return createTree(ordered, ordered[0]);
 };
