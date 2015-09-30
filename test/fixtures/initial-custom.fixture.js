93df452daacaea07f8e41db80ec7ef02a45fd5a2
Philipp Alferov <philipp.alferov@gmail.com>
2015-09-30 11:49:01 -0400

    Refine fixtures

--- test/fixtures/initial-custom.fixture.js
+++ test/fixtures/initial-custom.fixture.js
@@ -1,13 +1,13 @@
 module.exports = [{
-  _id: 1,
+  _id: 'ec654ec1-7f8f-11e3-ae96-b385f4bc450c',
   parent: null
 }, {
-  _id: 2,
-  parent: 1
+  _id: 'ec666030-7f8f-11e3-ae96-0123456789ab',
+  parent: 'ec654ec1-7f8f-11e3-ae96-b385f4bc450c'
 }, {
-  _id: 3,
-  parent: 2
+  _id: 'ec66fc70-7f8f-11e3-ae96-000000000000',
+  parent: 'ec666030-7f8f-11e3-ae96-0123456789ab'
 }, {
-  _id: 4,
+  _id: '32a4fbed-676d-47f9-a321-cb2f267e2918',
   parent: null
 }];
